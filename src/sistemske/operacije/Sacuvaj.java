a58cad8b87bd0da3a0528a57a3f1fe37f4d44b4e
Filip <djordjevic.filip994@gmail.com>
2017-07-09 20:50:23 +0200

    final

--- src/sistemske/operacije/Sacuvaj.java
+++ src/sistemske/operacije/Sacuvaj.java
@@ -17,7 +17,6 @@ public class Sacuvaj extends AbstractSistemskaOperacija{
 
     @Override
     protected void proveriPreduslove(Object object) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
